8e9d7ed9093402c42622040222e015fa4d332c75
y-it <79666579+wcastellan@users.noreply.github.com>
2021-11-28 10:11:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
