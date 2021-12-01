f07b308d39b966289c28df114494394fa04bcfd0
Wyatt C <wcastellan@yahoo.com>
2021-12-01 14:35:55 -0500

    add dist and utils folder.  Create promises

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site.js')
 const generatePage = require('./src/page-template.js');
+
 
 //const pageHTML = generatePage(name, github);
 
@@ -139,19 +140,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-         const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw new Error(err);
-
-          console.log('Page created! Check out index.html in this directory to see it!');
-          });
+         return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-
-
-//fs.writeFile('./index.html', pageHTML, err => {
-   // if (err) throw err;
-
-   // console.log('Portfolio complete! Check out index.html to see the output!');
-//});
-
